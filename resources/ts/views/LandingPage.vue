01e8b3183e4012cdcfb086cb5caeec686c8753f5
Vehikl Team <go@vehikl.com>
2019-09-19 16:44:07 -0400

    Add RegisterForm to landing page

--- resources/ts/views/LandingPage.vue
+++ resources/ts/views/LandingPage.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
         This is the Landing Page
+        <register-form></register-form>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator"
+    import RegisterForm from "../components/RegisterForm.vue";
 
+    @Component({
+        components: {RegisterForm}
+    })
     export default class LandingPage extends Vue {
     };
 </script>
