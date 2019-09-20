48d90852821a437a3dee1e5766d3a59c53d1c289
Vehikl Team <go@vehikl.com>
2019-09-20 17:01:04 -0400

    Remove RedirectIfAuthenticated Middleware, since this is an SPA

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ Route::get('/{any}', function () {
     return view('spa-entry-point');
 })->where('any', '.*');
 
-Route::post('/register', 'Auth\RegisterController@create')->middleware('guest');
+Route::post('/register', 'Auth\RegisterController@create');
