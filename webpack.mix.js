ce2afe38f5f03775ca0207ddddb40ca801a7ce17
Vehikl Team <go@vehikl.com>
2019-10-16 16:38:58 -0400

    Use sourcemaps

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
 
 mix.ts('resources/ts/app.ts', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
