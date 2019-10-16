5db91a7bf8a5d9d94ebc89e4478d736c8633492b
Vehikl Team <go@vehikl.com>
2019-10-16 16:38:17 -0400

    Add messageForm to landing page

--- resources/ts/views/LandingPage.vue
+++ resources/ts/views/LandingPage.vue
@@ -2,15 +2,17 @@
     <div>
         This is the Landing Page
         <register-form></register-form>
+        <message-form></message-form>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator"
     import RegisterForm from "../components/RegisterForm.vue";
+    import MessageForm from "../components/MessageForm.vue";
 
     @Component({
-        components: {RegisterForm}
+        components: {MessageForm, RegisterForm}
     })
     export default class LandingPage extends Vue {
     };
