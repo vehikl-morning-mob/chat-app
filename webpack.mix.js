c11f0d3f569700f88b7d671134f2858cb4ec632d
Vehikl Team <go@vehikl.com>
2019-10-30 15:42:50 -0400

    Bring tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
+
 mix.ts('resources/ts/app.ts', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .tailwind()
+    .purgeCss()
     .sourceMaps();
