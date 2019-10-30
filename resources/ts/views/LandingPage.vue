44caf81d9e2163882bf940f96f352c4861fc6606
Vehikl Team <go@vehikl.com>
2019-10-30 16:44:10 -0400

    cleanup landing page

--- resources/ts/views/LandingPage.vue
+++ resources/ts/views/LandingPage.vue
@@ -1,22 +1,13 @@
 <template>
-    <div>
-        This is the Landing Page
-        <login-form></login-form>
-        <register-form></register-form>
-        <chat-history></chat-history>
-        <message-form></message-form>
-    </div>
+    <login-form class="mt-20"></login-form>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator"
-    import RegisterForm from "../components/RegisterForm.vue";
-    import MessageForm from "../components/MessageForm.vue";
-    import ChatHistory from "../components/ChatHistory.vue";
     import LoginForm from "../components/LoginForm.vue";
 
     @Component({
-        components: {LoginForm, ChatHistory, MessageForm, RegisterForm}
+        components: {LoginForm}
     })
     export default class LandingPage extends Vue {
     };
